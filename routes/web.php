292ffc76e37603e9689bb9fc3a1550830fe18cef
pangestuasli <pangestu24si@mahasiswa.pcr.ac.id>
2025-10-08 11:17:23 +0700

    volt

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/anggota', function () {
     return view('anggota');
 });
+
 Route::get('/ketua', function () {
     return view('ketua');
 });
+
+Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
 
